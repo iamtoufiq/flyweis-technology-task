77981bfeb8fce270eafa29f2638816fb9220c89e
Toufiq Choudhari <toufiq.c@finzmoney.com>
2024-02-14 17:05:30 +0530

    Optimize Dashboard component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RiMenu4Fill } from "react-icons/ri";
 const Header = () => {
   return (
-    <div className="w-full bg-[#00AB7F] text-white flex justify-between items-center px-3 py-2 md:py-4  md:px-4 md:py-3">
+    <div className="w-full bg-[#00AB7F] text-white flex justify-between items-center px-3 py-3 md:py-4  md:px-4 ">
       <div className="transform -translate-x-180 rotate-x-180 cursor-pointer">
         <RiMenu4Fill size={22} />
       </div>
