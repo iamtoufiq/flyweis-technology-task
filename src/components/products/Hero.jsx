23e5e7b57f02888451d4837f92320a7d63dbed86
Toufiq Choudhari <toufiq.c@finzmoney.com>
2024-02-16 15:54:33 +0530

    Implemented api & created UI

--- src/components/products/Hero.jsx
+++ src/components/products/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="flex items-center justify-between w-full md:w-3/4 lg:w-2/5">
+    <div className="flex items-center justify-between gap-1 w-full md:w-3/4 lg:w-2/5">
       <h3 className="text-[18px] font-bold  border-b-[3px] border-[#00ab7f] inline">
         Girorganic
       </h3>
